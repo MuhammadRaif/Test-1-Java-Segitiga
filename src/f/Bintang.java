0b70f99c6d914063de9e41f2dc90affdf326c256
MuhammadRaif <raifcindyvers@gmail.com>
2018-09-06 21:05:01 +0700

    Tugas Uji Coba 1

---
+++ src/f/Bintang.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package f;
+
+/**
+ *
+ * @author MRA
+ */
+public class Bintang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String [] args){
+        int jumlah=5;
+        for (int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    
+}
